1d34abf17696f5572deb97123af3336dd6fac36d
Casper da Costa-Luis <j-rn@cdcl.ml>
2021-04-04 15:31:37 +0100

    codefolding tooltips

--- _static/custom.js
+++ _static/custom.js
@@ -1,15 +1,23 @@
 document.addEventListener("DOMContentLoaded", function(){
   var coll = document.getElementsByClassName("nbinput");
   for (var i = 0; i < coll.length; i++) {
-    var prompt = coll[i].children[0].children[0].children[0];
+    var highlight = coll[i].children[0].children[0];
+    highlight.children[0].children[0].innerHTML = "🔍";
+    let tooltip = document.createElement('span');
+    tooltip.textContent = "Toggle source";
+    tooltip.classList.toggle("tooltip-text");
+    highlight.appendChild(tooltip);
+
     coll[i].children[1].style.display = "none";
-    prompt.addEventListener("click", function() {
-      this.parentElement.classList.toggle("highlight-active");
-      var input_area = this.parentElement.parentElement.nextElementSibling;
+    highlight.addEventListener("click", function() {
+      this.classList.toggle("highlight-active");
+      var input_area = this.parentElement.nextElementSibling;
       if (input_area.style.display === "block") {
         input_area.style.display = "none";
+        this.children[0].children[0].innerHTML = "🔍";
       } else {
         input_area.style.display = "block";
+        this.children[0].children[0].innerHTML = "🚩";
       }
     });
   }
